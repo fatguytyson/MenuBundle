b2ecd21abec89bf0639fc9a1a16313eb6b69c3cb
Tyson Green <tyson@fatguyconsulting.com>
2018-01-20 17:46:02 -0800

    RouteOptions Import Error

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -44,6 +44,7 @@ class Configuration implements ConfigurationInterface
                             ->end()
                             ->variableNode('routeOptions')
                                 ->info('Route options if route needs it.')
+	                        ->end()
                             ->scalarNode('icon')
                                 ->info('Icon name to add in dashboard menus.')
                             ->end()
